71f4d0087e20cc63e20072cb3f4cbc7126c5775f
Jouliet Morales <jouliet.25@gmai.com>
2020-09-17 16:44:09 -0500

    Database connection added.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import connectDatabase from './config/db'
 
 //Initialize express app
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.get('/', (req, res) => 
     res.send('Http get request sent to root API endpoint.')
 );
+
+connectDatabase();
 
 //
 app.listen(3000, () => console.log('Server started at port 3000.'))
