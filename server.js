a6e190f065ec6778a569d6e994306cab6f9c3139
Jouliet Morales <jouliet.25@gmai.com>
2020-09-28 17:13:12 -0500

    Adding user endpoint and validator dependency.

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
-import express from 'express'
-import connectDatabase from './config/db'
+import express from 'express';
+import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
-//Initialize express app
+// Initialize express app
 const app = express();
+
+// Confi. Middlaware
+app.use(express.json({extended: false}));
 
 //API endoint
 app.get('/', (req, res) => 
     res.send('Http get request sent to root API endpoint.')
 );
 
+// Api/users - validation code - check errors
+app.post(
+    '/api/users', 
+[
+    check(
+        'name', 
+        'Please enter your name')
+        .not()
+        .isEmpty(),
+    check(
+        'email', 
+        'Please enter a valid email')
+        .isEmail(),
+    check(
+        'password', 
+        'Please enter a password with a 6 or more characters'
+    ).isLength({min: 6 })
+],
+(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errors: errors.array() }); 
+    } else {
+        return res.send(req.body);
+    }
+}
+);
+
 connectDatabase();
 
-//
+// Connection listener
 app.listen(3000, () => console.log('Server started at port 3000.'))
