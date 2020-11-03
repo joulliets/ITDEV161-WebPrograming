ef01fb226a35ffe95196243862cef3eabff9a3a6
Jouliet Morales <jouliet.25@gmai.com>
2020-11-03 15:47:54 -0600

    Completed Activity 7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
@@ -22,11 +24,33 @@ class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header"
-        >GoodThings</header>
-        {this.state.data}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>GoodThings</h1>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </ul>
+          </header>
+          <main>
+            <Route exact path="/">
+              {this.state.data}
+            </Route>
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
