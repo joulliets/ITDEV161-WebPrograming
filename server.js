e188bce368cdae0ec2b54af44a42f06676ebed7a
Jouliet Morales <jouliet.25@gmai.com>
2020-11-16 23:05:37 -0600

    Completed Activity 9

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
-import User from './models/User';
 import jwt, { sign } from 'jsonwebtoken';
 import config from 'config';
+import User from './models/User';
+import Post from './models/Post'
 import auth from './middleware/auth';
+
 
 
 // Initialize express app
@@ -156,6 +158,147 @@ const returnToken = (user, res) => {
     }
   );
 };
+
+// Post endpoints
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+  '/api/posts',
+  [
+    auth,
+    [
+      check('title', 'Title text is required')
+        .not()
+        .isEmpty(),
+      check('body', 'Body text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      const { title, body } = req.body;
+      try {
+        // Get the user who created the post
+        const user = await User.findById(req.user.id);
+
+        // Create a new post
+        const post = new Post({
+          user: user.id,
+          title: title,
+          body: body
+        });
+
+        // Save to the db and return
+        await post.save();
+
+        res.json(post);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+      }
+    }
+  }
+);
+
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+app.get('/api/posts', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route GET api/posts/:id
+ * @desc Get post
+ */
+app.get('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Make sure the request user created the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route PUT api/posts/:id
+ * @desc Update a post
+ */
+app.put('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Make sure the request user created the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Update the post and return
+    post.title = title || post.title;
+    post.body = body || post.body;
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
 
 //Connection Listener
 const port = 5000;
